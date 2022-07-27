fa25f5b47cfbdac7be02ed5a0ae6b9fd76b018eb
tamimhasan19702 <tamimhasancu@gmail.com>
2022-07-28 00:57:01 +0600

    geolocation api with city and lat lon add

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 const wrapper = document.querySelector('.wrapper'),
       inputPart = document.querySelector('.input-part'),
       infoText = document.querySelector('.info-txt'),
-      inputField = document.querySelector('input');
+      inputField = document.querySelector('input'),
+      locationBtn = document.querySelector('button');
+
+let api;      
+
+const apiKey = '9ca26e4d8f1c41111c555269728216c2';    
 
 inputField.addEventListener('keyup' , e => {
     if(e.key === "Enter" && inputField.value !== ''){
-        requestApi(inputField.value);
+      requestApi(inputField.value);
     }
-})      
+});      
+
+
+locationBtn.addEventListener("click", () => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser donot support geolocation api!!")
+    }
+})
+
+
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    fetchData();
+}
+
+function onError(error){
+    infoText.innerText = error.message;
+    infoText.classList.add('error');
+}
+
+
+
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    fetchData();
+}
+
+function fetchData(){
+    infoText.innerText = "Getting weather details...";
+    infoText.classList.add('pending');
+
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info){
+ console.log(info);
+}
